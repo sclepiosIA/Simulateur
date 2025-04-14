077d18e511a469da4ea67339f037884ab3324f3e
sclepiosIA <131526051+sclepiosIA@users.noreply.github.com>
2025-04-14 13:25:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,7 @@ import os
 import tempfile
 
 st.set_page_config(page_title="Simulateur Urgences - Sclépios I.A.", layout="wide")
-
-# Affichage du logo depuis un fichier local uniquement
-st.markdown("<div style='text-align: center;'><img src='logo_complet.png' width='250'></div>")
+st.markdown(f"<img src='{logo_path}' width='250'></div>", unsafe_allow_html=True)
 
 st.markdown("<br><h1 style='text-align: center;'>📊 Simulateur de Valorisation des Urgences</h1><br>", unsafe_allow_html=True)
 
